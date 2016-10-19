4edfd94109e88f21aa6ba72c64ca8851688e2256
Michael Ruf <michael.ruf@inxmail.de>
2016-10-19 09:56:57 +0200

    use correct spock dependency, moved keys to fixtures folder

--- src/test/groovy/de/agitos/dkim/FullExampleSpec.groovy
+++ src/test/groovy/de/agitos/dkim/FullExampleSpec.groovy
@@ -6,4 +6,14 @@ import spock.lang.Specification
 
 class FullExampleSpec extends Specification {
 
+    def "send full mail"(){
+        given:
+            DKIMSigner signer = new DKIMSigner( "mydomain.com", "default", "src/test/fixtures/keys/private.key.der" )
+        when:
+            signer
+        then:
+            signer
+
+    }
+
 }
